1a7d86a84d55fa0c2188fe352f83c23eb0887751
Gonzalo Fuentes <gonza.a.fuentes@gmail.com>
2020-07-06 07:05:00 -0300

    ads

--- server.js
+++ server.js
@@ -2,10 +2,10 @@ const express = require('express');
 
 const app = express();
 
-app.use(express.static('../dist/green-minds'));
+app.use(express.static(''));
 
 app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/green-minds/'}),
+    res.sendFile('index.html', {root: ''}),
 );
 
 app.listen(process.env.PORT || 8080);
